bfa67efb07e524394f7db69adc25b4b9e97dfec7
Noreldin-S <71359506+Noreldin-S@users.noreply.github.com>
2021-01-23 17:41:07 -0800

    first

---
+++ Js/Questions.js
@@ -0,0 +1,28 @@
+var questions = [
+  {
+    title: "Who are you watching with?",
+    choices: [
+      "Alone",
+      "Wife", 
+      "Friends",
+      "Family"],
+  },
+  {
+    title: "What is your Emotions?",
+    choices: [
+      "Happy",
+      "Depressed",
+      "Excited",
+      "Whatever"],
+  },
+  {
+    title: "What do you feel like watching?",
+    choices: [
+      "Comedy",
+      "Romantic",
+      "Action",
+      "Horror"
+    ],
+  },
+]
+
